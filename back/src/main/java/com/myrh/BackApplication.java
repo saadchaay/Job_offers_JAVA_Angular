9e08ec9c088c218d8e3d88691e3afade131eac5f
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2023-01-12 15:18:52 +0000

    delete useless method

--- back/src/main/java/com/myrh/BackApplication.java
+++ back/src/main/java/com/myrh/BackApplication.java
@@ -16,18 +16,4 @@ public class BackApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackApplication.class, args);
     }
-
-//    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-        config.setAllowedMethods(Arrays.asList("*"));
-        config.setAllowedHeaders(Arrays.asList("*", "Authorization"));
-        config.setExposedHeaders(Arrays.asList("content-length", "content-disposition"));
-        config.setMaxAge(3600L);
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
 }
